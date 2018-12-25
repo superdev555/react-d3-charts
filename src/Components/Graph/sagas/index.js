f3cdead8b43d6f7307614206a68c5f978fab74ee
hryhorii515 <hryhorii.ohanisian@gmail.com>
2018-12-25 04:28:57 -0800

    Restructure.

--- src/Components/Graph/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-
-import watchGetGraphDataSaga from './watchers/getGraphData';
-
-export default function* root() {
-  yield all([
-    fork(watchGetGraphDataSaga),
-  ]);
-}
